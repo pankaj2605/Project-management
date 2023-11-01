27197cb094b9f09269836b1d7451281b64803cfd
Pankaj Kumar <pankajs2167@gmal.com>
2023-11-01 13:33:14 +0530

    adding project task and task component

---
+++ src/components/NewTask.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from './Button'
+
+export default function NewTask() {
+  return (
+    <div className='flex items-center gap-4'>
+        <input type="text" className='w-64 px-2 py-1 rounded-sm bg-stone-200'/>
+        <Button >Add Task</Button>
+    </div>
+  )
+}
--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Tasks from './Tasks.jsx';
+import Button from './Button.jsx';
 
 export default function SelectedProject({project,onDelete}) {
     const formattedDate =new Date(project.dueDate).toLocaleDateString('en-US',{
@@ -11,16 +13,16 @@ export default function SelectedProject({project,onDelete}) {
         <header className='pb-4 mb-4 border-b-2 border-stone-300'>
             <div className='flex items-center justify-between'>
                 <h1 className='text-3xl font-bold text-stone-600 mb-2'>{project.title}</h1>
-                <button className='text-stone-600 hover:text-stone-950' onClick={onDelete}>
+                <Button  onClick={onDelete}>
                     Delete
-                </button>
+                </Button>
             </div>
             <div>
                 <p className='mb-4 text-stone-400'>{formattedDate}</p>
                 <p className='text-stone-600 whitespace-pre-wrap'>{project.description}</p>
             </div>
         </header>
-        TASKS
+        <Tasks/>
     </div>
   )
 }
