b365c102507c6ec7404fe739c129d3c13784ce63
Pankaj Kumar <pankajs2167@gmal.com>
2023-11-01 08:29:05 +0530

    styling button and input with tailwind

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -3,13 +3,13 @@ import Input from './Input'
 
 export default function NewProject() {
   return (
-    <div>
-        <menu>
+    <div className='w-[35rem] mt-16'>
+        <menu className='flex items-center justify-end gap-4 my-4'>
             <li>
-                <button>Cancel</button>
+                <button className='px-6 py-2 rounded-md text-slate-800 hover:text-slate-950 hover:bg-red-400'>Cancel</button>
             </li>
             <li>
-                <button>Save</button>
+                <button className= ' px-6 py-2 rounded-md bg-slate-600 text-stone-50 hover:bg-slate-950'>Save</button>
             </li>
         </menu>
         <div>
