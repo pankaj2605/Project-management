63b33dc41dc1b251b5731ff8e0ba02ac4738ab0e
Pankaj Kumar <pankajs2167@gmal.com>
2023-11-01 11:48:54 +0530

    styling the modal eith css

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,16 @@ function App() {
     })
   }
 
+  const handleCancelAddProject=()=>{
+      setProjectState(prevState=>{
+        return{
+          ...prevState,
+          selectedProjectId:undefined,
+
+        }
+      }
+      )
+   }
   const handleAddProject=(projectData)=>{
 
     setProjectState(prevState=>{
@@ -40,7 +50,7 @@ function App() {
   let content;
 
   if(projectState.selectedProjectId === null){
-        content = <NewProject onAdd={handleAddProject} />
+        content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
       } else if (projectState.selectedProjectId === undefined){
         content =  <NoProjectSelected onStartAddProject={handleStartAddProject} />;
       }
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react'
 import Input from './Input'
 import Modal from './Modal';
 
-export default function NewProject({onAdd}) {
+export default function NewProject({onAdd,onCancel}) {
     const modal=useRef();
     const title=useRef();
     const description =useRef();
@@ -29,14 +29,14 @@ export default function NewProject({onAdd}) {
   return (
     <>
         <Modal ref={modal} buttonCaption="Okay">
-            <h2>Invalid Input</h2>
-            <p>Oops ... looks like you forgot to enter a value</p>
-            <p>Please make sure you provide a valid value for every input field. </p>
+            <h2  className='text-xl font-bold text-stone-700 my-4' >Invalid Input</h2>
+            <p className='text-stone-600 mb-4'>Oops ... looks like you forgot to enter a value</p>
+            <p className='text-stone-600 mb-4'>Please make sure you provide a valid value for every input field. </p>
         </Modal>
     <div className='w-[35rem] mt-16'>
         <menu className='flex items-center justify-end gap-4 my-4'>
             <li>
-                <button className='px-6 py-2 rounded-md text-slate-800 hover:text-slate-950 hover:bg-red-400'>Cancel</button>
+                <button className='px-6 py-2 rounded-md text-slate-800 hover:text-slate-950 hover:bg-red-400' onClick={onCancel}>Cancel</button>
             </li>
             <li>
                 <button className= ' px-6 py-2 rounded-md bg-slate-600 text-stone-50 hover:bg-slate-950' onClick={handleSave}>Save</button>
