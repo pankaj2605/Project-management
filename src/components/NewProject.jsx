aa8f19adf63b974cdc9cce79dd901e72c41d7e80
Pankaj Kumar <pankajs2167@gmal.com>
2023-11-01 10:04:20 +0530

    collecting user iput and updating in UI

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Input from './Input'
 
-export default function NewProject() {
+export default function NewProject({onAdd}) {
+    const title=useRef();
+    const description =useRef();
+    const dueDate =useRef();
+
+    const handleSave=()=>{
+        const enteredTitle = title.current.value;
+        const enteredDescription = description.current.value;
+        const enteredDueDate = dueDate.current.value;
+
+        //vadlidation
+
+        onAdd({
+            title:enteredTitle,
+            description:enteredDescription,
+            dueDate:enteredDueDate
+        });
+    }
   return (
     <div className='w-[35rem] mt-16'>
         <menu className='flex items-center justify-end gap-4 my-4'>
@@ -9,13 +26,13 @@ export default function NewProject() {
                 <button className='px-6 py-2 rounded-md text-slate-800 hover:text-slate-950 hover:bg-red-400'>Cancel</button>
             </li>
             <li>
-                <button className= ' px-6 py-2 rounded-md bg-slate-600 text-stone-50 hover:bg-slate-950'>Save</button>
+                <button className= ' px-6 py-2 rounded-md bg-slate-600 text-stone-50 hover:bg-slate-950' onClick={handleSave}>Save</button>
             </li>
         </menu>
         <div>
-            <Input label="Title"/>
-            <Input textarea label="Description"/>
-            <Input label="Due Date" />
+            <Input type="text" ref={title} label="Title" />
+            <Input ref={description} textarea label="Description"/>
+            <Input type="date" ref={dueDate} label="Due Date" />
             
         </div>
     </div>
