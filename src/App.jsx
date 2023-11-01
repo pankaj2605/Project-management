dcba676216b85a145b27334bc28ee5f2291381ea
Pankaj Kumar <pankajs2167@gmal.com>
2023-11-01 09:33:50 +0530

    Mangaging state to swtich between components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
 import NewProject from "./components/Newproject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectState,setProjectState]=useState({
+    selectedProjectId:undefined,
+    projects:[]
+  });
+
+  const handleStartAddProject=()=>{
+    setProjectState(prevState=>{
+      return{
+        ...prevState,
+        selectedProjectId:null,
+
+      }
+    })
+  }
+
+  let content;
+
+  if(projectState.selectedProjectId === null){
+        content = <NewProject/>
+      } else if (projectState.selectedProjectId === undefined){
+        content =  <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+      }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar/>
-      <NoProjectSelected/>
+      <ProjectsSidebar onStartAddProject={handleStartAddProject}/>
+      {content}
       {/* <NewProject/> */}
     </main>
   );
